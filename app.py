f0ccc830d7cd66f4693157ed65d8711dea0ecc17
Alex <64358227+ARCIKE@users.noreply.github.com>
2022-12-02 03:33:45 +0000

    Pending changes exported from your codespace

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, send_file
+import os
+import uuid
+from urllib import request
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return render_template("index.html", title="Hello")
+@app.get("/<path:path>")
+def download (path):
+    pdfuuid = str(uuid.uuid4())
+    URL = path
+    path = "static/" + pdfuuid + ".pdf"
+    response = request.urlretrieve(URL, path)
+    os.system("pdf2htmlEX " + path + " ./static/" + pdfuuid + ".html")
+    os.remove(path)
+    return send_file("static/" + pdfuuid + ".html", as_attachment=True)
